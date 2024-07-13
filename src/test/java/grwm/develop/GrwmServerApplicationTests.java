e4408b5d7210c6ab325a55917f3496ac9e3814af
MinseokGo <rhalstjr1999@naver.com>
2024-07-13 16:45:30 +0900

    init: project

---
+++ src/test/java/grwm/develop/GrwmServerApplicationTests.java
@@ -0,0 +1,12 @@
+package grwm.develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
