5c902030647abe96271765de00f51aa324d76031
MinseokGo <rhalstjr1999@naver.com>
2024-07-20 17:38:20 +0900

    🗑️ remove: 구글 로그인 삭제

--- src/main/java/grwm/develop/member/Member.java
+++ src/main/java/grwm/develop/member/Member.java
@@ -2,32 +2,16 @@ package grwm.develop.member;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
-@ToString
-@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Member extends BaseEntity {
 
     @Column(nullable = false)
     private String name;
@@ -38,20 +22,4 @@ public class Member {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Service service;
-
-    @CreatedDate
-    private LocalDateTime createAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
-
-    @Column(columnDefinition = "boolean default false")
-    private boolean isDeleted;
-
-    @Builder
-    public Member(String name, String email, Service service) {
-        this.name = name;
-        this.email = email;
-        this.service = service;
-    }
 }
