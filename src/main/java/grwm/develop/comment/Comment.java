58ace35f5bc82a0c9a0f65e99e40e42c42527178
MinseokGo <rhalstjr1999@naver.com>
2024-07-20 17:28:58 +0900

    🚀 feat: image, comment 엔티티

--- src/main/java/grwm/develop/comment/Comment.java
+++ src/main/java/grwm/develop/comment/Comment.java
@@ -1,11 +1,10 @@
-package grwm.develop.recipe;
+package grwm.develop.comment;
 
 import grwm.develop.BaseEntity;
 import grwm.develop.member.Member;
+import grwm.develop.recipe.Recipe;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,22 +17,16 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Recipe extends BaseEntity {
+public class Comment extends BaseEntity {
 
-    @Column(length = 128, nullable = false)
-    private String title;
-
-    @Column(length = 2048, nullable = false)
+    @Column(length = 512, nullable = false)
     private String content;
 
     @JoinColumn(name = "member_id")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private Member member;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Category category;
-
-    @Column(nullable = false)
-    private boolean isPublic;
+    @JoinColumn(name = "recipe_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Recipe recipe;
 }
--- src/main/java/grwm/develop/image/Image.java
+++ src/main/java/grwm/develop/image/Image.java
@@ -1,11 +1,9 @@
-package grwm.develop.recipe;
+package grwm.develop.image;
 
 import grwm.develop.BaseEntity;
-import grwm.develop.member.Member;
+import grwm.develop.recipe.Recipe;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,22 +16,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Recipe extends BaseEntity {
-
-    @Column(length = 128, nullable = false)
-    private String title;
+public class Image extends BaseEntity {
 
     @Column(length = 2048, nullable = false)
-    private String content;
+    private String url;
 
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "recipe_id")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private Member member;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Category category;
-
-    @Column(nullable = false)
-    private boolean isPublic;
+    private Recipe recipe;
 }
