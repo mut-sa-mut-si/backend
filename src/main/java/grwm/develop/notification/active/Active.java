eec7fc49fb4333da0ebc6f8bc3e9a50ad5185ca7
MinseokGo <rhalstjr1999@naver.com>
2024-07-21 20:15:54 +0900

    🚀 feat: 알림 엔티티 추가

---
+++ src/main/java/grwm/develop/notification/active/Active.java
@@ -0,0 +1,28 @@
+package grwm.develop.notification.active;
+
+import grwm.develop.BaseEntity;
+import grwm.develop.member.Member;
+import grwm.develop.recipe.Recipe;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Active extends BaseEntity {
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Member member;
+
+    @JoinColumn(name = "recipe_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Recipe recipe;
+}
---
+++ src/main/java/grwm/develop/notification/change/Change.java
@@ -0,0 +1,27 @@
+package grwm.develop.notification.change;
+
+import grwm.develop.BaseEntity;
+import grwm.develop.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Change extends BaseEntity {
+
+    private int pastPrice;
+
+    private int afterPrice;
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Member member;
+}
