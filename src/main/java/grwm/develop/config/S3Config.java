5b29ce3e7aa2744ba34f5bf22caed56c180c07b6
MinseokGo <rhalstjr1999@naver.com>
2024-07-22 02:26:10 +0900

    🔨 refactor: 설정값 정리

--- src/main/java/grwm/develop/config/S3Config.java
+++ src/main/java/grwm/develop/config/S3Config.java
@@ -4,27 +4,29 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import grwm.develop.utils.S3Properties;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
+    private static final String ACCESS_KEY = "accessKey";
+    private static final String SECRET_KEY = "secretKey";
+    private static final String STATIC = "static";
 
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
+    private final S3Properties s3Properties;
 
     @Bean
     public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        Map<String, String> credentials = s3Properties.getCredentials();
+        BasicAWSCredentials awsCredentials =
+                new BasicAWSCredentials(credentials.get(ACCESS_KEY), credentials.get(SECRET_KEY));
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+                .withRegion(s3Properties.getRegion().get(STATIC))
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
