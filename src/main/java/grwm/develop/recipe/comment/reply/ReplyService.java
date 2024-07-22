ece99c5a93c37c7f6e818d06ff71d7b9c5d92963
ayeon598 <165542857+ayeon598@users.noreply.github.com>
2024-07-23 02:16:04 +0900

    🔨 refactor: 트랜잭션 추가, 로그 삭제

--- src/main/java/grwm/develop/recipe/comment/reply/ReplyService.java
+++ src/main/java/grwm/develop/recipe/comment/reply/ReplyService.java
@@ -3,14 +3,13 @@ package grwm.develop.recipe.comment.reply;
 import grwm.develop.member.Member;
 import grwm.develop.recipe.comment.Comment;
 import grwm.develop.recipe.comment.CommentRepository;
-import grwm.develop.recipe.comment.reply.dto.WriteReplyRequest;
 import grwm.develop.recipe.comment.reply.dto.ReplyResponse;
+import grwm.develop.recipe.comment.reply.dto.WriteReplyRequest;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ReplyService {
@@ -18,6 +17,7 @@ public class ReplyService {
     private final ReplyRepository replyRepository;
     private final CommentRepository commentRepository;
 
+    @Transactional
     public ReplyResponse writeReply(Long commentId, Member member, WriteReplyRequest request) {
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(EntityNotFoundException::new);
