7be7f3e30f9264fd21564b2fc28d64ed9ca62378
MinseokGo <rhalstjr1999@naver.com>
2024-07-22 10:56:53 +0900

    📝 rename: WebConfig 파일 이동

--- src/main/java/grwm/develop/config/WebConfig.java
+++ src/main/java/grwm/develop/config/WebConfig.java
@@ -1,4 +1,4 @@
-package grwm.develop.auth.security;
+package grwm.develop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
