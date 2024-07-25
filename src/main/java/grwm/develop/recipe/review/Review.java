c1721fc82f6e1af752db2c7c552acdeac5d6fc84
MinseokGo <rhalstjr1999@naver.com>
2024-07-25 21:31:07 +0900

    🚀 feat: Review 엔티티 등록

---
+++ src/main/java/grwm/develop/recipe/review/Review.java
@@ -0,0 +1,34 @@
+package grwm.develop.recipe.review;
+
+import grwm.develop.BaseEntity;
+import grwm.develop.member.Member;
+import grwm.develop.recipe.Recipe;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends BaseEntity {
+
+    @Column(length = 2048, nullable = false)
+    private String content;
+
+    private float rating;
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Member member;
+
+    @JoinColumn(name = "recipe_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Recipe recipe;
+}
