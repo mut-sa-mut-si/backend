840145d44e7c1442b7d528cf0ed4552c59959948
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-07-26 02:10:25 +0900

    🔍 typo: Scap리포지토리 오탈자 수정

--- src/main/java/grwm/develop/recipe/scrap/ScarpRepository.java
+++
@@ -1,6 +0,0 @@
-package grwm.develop.recipe.scrap;
-
-import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
-
-public interface ScarpRepository extends JpaAttributeConverter<Scrap,Long> {
-}
---
+++ src/main/java/grwm/develop/recipe/scrap/ScrapRepository.java
@@ -0,0 +1,7 @@
+package grwm.develop.recipe.scrap;
+
+import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScrapRepository extends JpaRepository<Scrap,Long> {
+}
