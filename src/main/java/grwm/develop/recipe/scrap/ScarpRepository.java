ab5204b94e0fb9e3e796debdf2dd123c0e2b27fc
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-07-26 02:07:59 +0900

    🚀 feat: Scrap 리포지토리 등록

---
+++ src/main/java/grwm/develop/recipe/scrap/ScarpRepository.java
@@ -0,0 +1,6 @@
+package grwm.develop.recipe.scrap;
+
+import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+
+public interface ScarpRepository extends JpaAttributeConverter<Scrap,Long> {
+}
