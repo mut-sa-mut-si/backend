7ea61afb03fcda2c99486f54fd633421a4917244
MinseokGo <rhalstjr1999@naver.com>
2024-07-26 01:43:00 +0900

    🚀 feat: 채팅방 조회 기능

--- src/main/java/grwm/develop/chat/ChatController.java
+++ src/main/java/grwm/develop/chat/ChatController.java
@@ -2,10 +2,12 @@ package grwm.develop.chat;
 
 import grwm.develop.auth.security.UserDetailsImpl;
 import grwm.develop.chat.dto.FindAllChatRoomsResponse;
+import grwm.develop.chat.dto.FindChatRoomResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +21,17 @@ public class ChatController {
 
     @GetMapping
     public ResponseEntity<FindAllChatRoomsResponse> findAll(@RequestParam(name = "category") String category,
-                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
+                                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         FindAllChatRoomsResponse response = chatService.findAllChats(category, userDetails.member());
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FindChatRoomResponse> find(@PathVariable(name = "id") Long id,
+                                     @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        FindChatRoomResponse response = chatService.findChat(id, userDetails.member());
         return ResponseEntity.ok().body(response);
     }
 }
