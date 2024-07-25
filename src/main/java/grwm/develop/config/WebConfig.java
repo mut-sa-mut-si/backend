4923f30e8223b04a2a2cbd930193f6a25a970fbe
MinseokGo <rhalstjr1999@naver.com>
2024-07-26 02:20:16 +0900

    🎨 style: 공백 라인 추가

--- src/main/java/grwm/develop/config/WebConfig.java
+++ src/main/java/grwm/develop/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
