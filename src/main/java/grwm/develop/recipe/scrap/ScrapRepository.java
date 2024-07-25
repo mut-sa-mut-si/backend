7bc290ab47955864a57aac6f103371605530c2ce
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-07-26 02:30:13 +0900

    🚀 feat: Scrap 리포지토리 findBymemberId추가

--- src/main/java/grwm/develop/recipe/scrap/ScrapRepository.java
+++ src/main/java/grwm/develop/recipe/scrap/ScrapRepository.java
@@ -4,4 +4,5 @@ import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScrapRepository extends JpaRepository<Scrap,Long> {
+    public Scrap findBymemberId(Long memberId);
 }
