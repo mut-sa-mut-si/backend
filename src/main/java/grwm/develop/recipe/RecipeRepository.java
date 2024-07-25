22c0041492ff4c6ac15694216c91a05d07bc7b2f
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-07-26 01:46:58 +0900

    🚀 feat: Repositoy에 find함수 추가
    HashRepositoy
    ImageRepositoy
    RecipeRepositoy
    ReviewRepositoy

--- src/main/java/grwm/develop/recipe/RecipeRepository.java
+++ src/main/java/grwm/develop/recipe/RecipeRepository.java
@@ -1,6 +1,11 @@
 package grwm.develop.recipe;
 
+import grwm.develop.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+    List<Recipe> findAllByMember(Long memberId);
 }
--- src/main/java/grwm/develop/recipe/image/ImageRepository.java
+++ src/main/java/grwm/develop/recipe/image/ImageRepository.java
@@ -2,5 +2,10 @@ package grwm.develop.recipe.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    List<Image> findAllByRecipe(Long recipeId);
+
 }
