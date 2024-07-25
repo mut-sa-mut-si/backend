3d01b6fbe7314d4fbb1654b1b547bdb1f91b67ee
MinseokGo <rhalstjr1999@naver.com>
2024-07-26 02:50:53 +0900

    🔨 refactor: 메시지 DTO 맵핑 방식 수정

--- src/main/java/grwm/develop/chat/ChatSocketHandler.java
+++ src/main/java/grwm/develop/chat/ChatSocketHandler.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +19,7 @@ public class ChatSocketHandler {
 
     @MessageMapping("/{roomId}/send")
     @SendTo("/chats/{roomId}")
-    public ResponseEntity<SendChatDTO> chat(@RequestBody SendChatDTO chat,
+    public ResponseEntity<SendChatDTO> chat(@Payload SendChatDTO chat,
                                             @DestinationVariable Long roomId,
                                             @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
