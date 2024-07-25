a17816cdd02f0fa29bd5f204bda82b0b86fd3cfe
MinseokGo <rhalstjr1999@naver.com>
2024-07-26 00:46:33 +0900

    🚀 feat: Chat 컨트롤러 등록

---
+++ src/main/java/grwm/develop/chat/ChatController.java
@@ -0,0 +1,27 @@
+package grwm.develop.chat;
+
+import grwm.develop.auth.security.UserDetailsImpl;
+import grwm.develop.chat.dto.FindAllChatRoomsResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/chats")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @GetMapping
+    public ResponseEntity<FindAllChatRoomsResponse> findAll(@RequestParam(name = "category") String category,
+                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        FindAllChatRoomsResponse response = chatService.findAllChats(category, userDetails.member());
+        return ResponseEntity.ok().body(response);
+    }
+}
