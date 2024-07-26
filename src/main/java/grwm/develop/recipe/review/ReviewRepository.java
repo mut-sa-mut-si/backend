f9007a77dce2a98775b89b530dcb07f1871098cb
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-07-26 11:21:15 +0900

    🚀 feat: 리포지토리 함수 추가
    Image
    Recipe
    Review

--- src/main/java/grwm/develop/recipe/review/ReviewRepository.java
+++ src/main/java/grwm/develop/recipe/review/ReviewRepository.java
@@ -2,5 +2,9 @@ package grwm.develop.recipe.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review,Long> {
+
+    List<Review> findAllByRecipeId(Long RecipeId);
 }
