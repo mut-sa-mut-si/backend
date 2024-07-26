54f42a7ffe5d3f9dc3d76871b90ce12d5e7f0c28
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-07-26 21:23:26 +0900

    🎨 style: 함수 반환형 변경
    findBymemberId

--- src/main/java/grwm/develop/recipe/scrap/ScrapRepository.java
+++ src/main/java/grwm/develop/recipe/scrap/ScrapRepository.java
@@ -3,6 +3,8 @@ package grwm.develop.recipe.scrap;
 import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
-    public Scrap findBymemberId(Long memberId);
+    public List<Scrap> findBymemberId(Long memberId);
 }
