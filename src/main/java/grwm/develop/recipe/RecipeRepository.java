d73bfa19fc29850f61ca5902421a3fbe693e0060
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-07-26 19:16:52 +0900

    🚀 feat: 리포지토리 함수 추가
    ImageRepository
    RecipeRepository

--- src/main/java/grwm/develop/recipe/RecipeRepository.java
+++ src/main/java/grwm/develop/recipe/RecipeRepository.java
@@ -2,5 +2,10 @@ package grwm.develop.recipe;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+    List<Recipe> findByTitleContainingOrContentContaining(String keyword);
+
 }
