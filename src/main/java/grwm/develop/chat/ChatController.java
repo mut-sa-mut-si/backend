d0004979a7b39421bc9086df517325ad4ac2dc6c
MinseokGo <rhalstjr1999@naver.com>
2024-07-27 00:13:20 +0900

    🚀 feat: 채팅방 입장 기능

--- src/main/java/grwm/develop/chat/ChatController.java
+++ src/main/java/grwm/develop/chat/ChatController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,14 @@ public class ChatController {
 
         FindChatRoomResponse response = chatService.findChat(id, userDetails.member());
         return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> participate(@RequestParam(name = "memberId") Long memberId,
+                                              @RequestParam(name = "category") String category,
+                                              @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        chatService.participateChat(memberId, userDetails.member(), category);
+        return ResponseEntity.ok().body("ok");
     }
 }
