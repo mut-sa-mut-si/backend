3ac7f98c9b2aea9e01ddcbe74ed37e9f63ec59ad
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-07-29 16:39:52 +0900

    🔨 refactor:  리포지토리 함수 추가
    ImageRepository

--- src/main/java/grwm/develop/recipe/image/ImageRepository.java
+++ src/main/java/grwm/develop/recipe/image/ImageRepository.java
@@ -2,6 +2,9 @@ package grwm.develop.recipe.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
+    List<Image> findAllByRecipeId(Long recipeId);
     Image findByRecipeId(Long recipeId);
 }
