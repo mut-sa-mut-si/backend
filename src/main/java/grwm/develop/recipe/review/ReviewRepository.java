b4170807bcf61a59ab06fb42a3ea21bf1ca9f448
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-07-29 16:55:08 +0900

    🐛 fix: 중복함수 제거
    ReviewRepository

--- src/main/java/grwm/develop/recipe/review/ReviewRepository.java
+++ src/main/java/grwm/develop/recipe/review/ReviewRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review,Long> {
-    List<Review> findAllByRecipe(Long recipeId);
     List<Review> findAllByRecipeId(Long RecipeId);
 }
