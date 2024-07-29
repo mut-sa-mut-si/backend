75b4b0e0052d9185c29423d79311debb9405714c
MinseokGo <rhalstjr1999@naver.com>
2024-07-29 19:48:27 +0900

    🔨 refactor: 트랜잭션 수정

--- src/main/java/grwm/develop/recipe/SearchService.java
+++ src/main/java/grwm/develop/recipe/SearchService.java
@@ -9,9 +9,11 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class SearchService {
 
     private final HashtagRepository hashtagRepository;
