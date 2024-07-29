52d657ae4e5da7573272d4a3b2e6917965b34253
MinseokGo <rhalstjr1999@naver.com>
2024-07-29 17:59:21 +0900

    🔨 refactor: fetch join 추가

--- src/main/java/grwm/develop/chat/ChatRepository.java
+++ src/main/java/grwm/develop/chat/ChatRepository.java
@@ -2,8 +2,10 @@ package grwm.develop.chat;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
+    @Query("select c from Chat c join fetch c.member join fetch c.room")
     List<Chat> findByRoomId(Long roomId);
 }
