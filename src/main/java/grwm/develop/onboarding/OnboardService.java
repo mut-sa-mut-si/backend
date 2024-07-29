c9f6293e781738025ddcb30af7cf2d1f67432c55
MinseokGo <rhalstjr1999@naver.com>
2024-07-29 18:22:13 +0900

    🚀 feat: 온보딩 키워드 저장 로직

--- src/main/java/grwm/develop/onboarding/OnboardService.java
+++ src/main/java/grwm/develop/onboarding/OnboardService.java
@@ -6,11 +6,15 @@ import grwm.develop.onboarding.dto.LandingMemberResponse;
 import grwm.develop.onboarding.dto.OnboardRequest;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class OnboardService {
+
+    private final OnboardRepository onboardRepository;
 
     public LandingMemberResponse landing(Member member) {
         return LandingMemberResponse.from(member);
@@ -22,6 +26,7 @@ public class OnboardService {
         addSkinOnboard(request, member, onboards);
         addHealthOnboard(request, member, onboards);
         addNutrientsOnboard(request, member, onboards);
+        onboardRepository.saveAll(onboards);
     }
 
     private void addSkinOnboard(OnboardRequest request, Member member, List<Onboard> onboards) {
