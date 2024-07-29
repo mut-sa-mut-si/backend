376f08aa963d3088297297f2c51a8b279f8e9cc0
MinseokGo <rhalstjr1999@naver.com>
2024-07-29 17:24:03 +0900

    🚀 feat: 온보딩 기능

--- src/main/java/grwm/develop/onboarding/OnboardController.java
+++ src/main/java/grwm/develop/onboarding/OnboardController.java
@@ -2,10 +2,13 @@ package grwm.develop.onboarding;
 
 import grwm.develop.auth.security.UserDetailsImpl;
 import grwm.develop.onboarding.dto.LandingMemberResponse;
+import grwm.develop.onboarding.dto.OnboardRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,13 @@ public class OnboardController {
     public ResponseEntity<LandingMemberResponse> landingPage(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         LandingMemberResponse response = onboardService.landing(userDetails.member());
         return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> onboardPage(@RequestBody OnboardRequest request,
+                                              @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        onboardService.onboarding(request, userDetails.member());
+        return ResponseEntity.ok().body("ok");
     }
 }
