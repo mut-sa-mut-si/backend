0333780f0e2dd0be7699d88d37c7f3b628eca0db
MinseokGo <rhalstjr1999@naver.com>
2024-07-29 18:14:03 +0900

    🐛 fix: 채팅방 조회 버그 수정

--- src/main/java/grwm/develop/chat/participant/ParticipantRepository.java
+++ src/main/java/grwm/develop/chat/participant/ParticipantRepository.java
@@ -2,13 +2,10 @@ package grwm.develop.chat.participant;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
 
-    @Query("select p from Participant p join fetch p.room join fetch p.member")
     List<Participant> findByMemberId(Long memberId);
 
-    @Query("select p from Participant p join fetch p.room join fetch p.member")
     List<Participant> findByRoomId(Long roomId);
 }
