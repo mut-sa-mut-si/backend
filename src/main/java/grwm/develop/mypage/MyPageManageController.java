c2ce8fedc887803e54545fc61a6d29c4e94484d2
MinseokGo <rhalstjr1999@naver.com>
2024-08-01 18:06:06 +0900

    🚀 feat: 구독 취소 기능

--- src/main/java/grwm/develop/mypage/MyPageManageController.java
+++ src/main/java/grwm/develop/mypage/MyPageManageController.java
@@ -5,6 +5,7 @@ import grwm.develop.mypage.dto.FindAllSubscribesResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +24,14 @@ public class MyPageManageController {
 
         FindAllSubscribesResponse response = myPageManageService.findAllSubscribes(userDetails.member());
         return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/{memberId}/subscribes/{subscribeId}")
+    public ResponseEntity<String> cancelSubscribe(@PathVariable(name = "memberId") Long memberId,
+                                                  @PathVariable(name = "subscribeId") Long subscribeId,
+                                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        myPageManageService.deleteSubscribe(subscribeId, userDetails.member());
+        return ResponseEntity.ok().body("ok");
     }
 }
