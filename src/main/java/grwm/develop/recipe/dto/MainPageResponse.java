2f2bab788ebedef03b06e23fcd3495474a252b02
MinseokGo <rhalstjr1999@naver.com>
2024-08-01 17:21:26 +0900

    🐛 fix: DTO 응답이 되지 않는 버그 수정

--- src/main/java/grwm/develop/recipe/dto/MainPageResponse.java
+++ src/main/java/grwm/develop/recipe/dto/MainPageResponse.java
@@ -1,57 +1,56 @@
 package grwm.develop.recipe.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
-public class MainPageResponse{
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MainPageResponse {
     int totalRecipeCount;
     Long joinDate;
     String profileMemberName;
     List<FindReciper> popularRecipers;
     List<RecommendRecipe> recommendRecipes;
     List<RecipeReview> recipeReviews;
-    public MainPageResponse(int totalRecipeCount, Long joinDate, String profileMemberName)
-    {
+
+    public MainPageResponse(int totalRecipeCount, Long joinDate, String profileMemberName) {
         this.totalRecipeCount = totalRecipeCount;
         this.joinDate = joinDate;
-        this.profileMemberName =profileMemberName;
+        this.profileMemberName = profileMemberName;
         popularRecipers = new ArrayList<>();
         recommendRecipes = new ArrayList<>();
         recipeReviews = new ArrayList<>();
     }
-    public void plusRecipers(List<FindReciper> findReciper)
-    {
+
+    public void plusRecipers(List<FindReciper> findReciper) {
         popularRecipers.addAll(findReciper);
     }
-    public void plusRecipes(List<RecommendRecipe> recommendRecipe)
-    {
+
+    public void plusRecipes(List<RecommendRecipe> recommendRecipe) {
         recommendRecipes.addAll(recommendRecipe);
     }
-    public void plusReviews(List<RecipeReview> recipeReview)
-    {
+
+    public void plusReviews(List<RecipeReview> recipeReview) {
         recipeReviews.addAll(recipeReview);
     }
+
     @Getter
     @AllArgsConstructor
-    public static class FindReciper
-    {
+    public static class FindReciper {
         int rank;
         int recipeCount;
         int reviewCount;
         float ratingAverage;
         FindMember member;
     }
+
     @Getter
     @AllArgsConstructor
-    public static class RecommendRecipe
-    {
+    public static class RecommendRecipe {
         Long id;
         String title;
         String image;
@@ -59,10 +58,10 @@ public class MainPageResponse{
         float ratingAverage;
         FindMember member;
     }
+
     @Getter
     @AllArgsConstructor
-    public static class RecipeReview
-    {
+    public static class RecipeReview {
         Long id;
         String content;
         float rating;
@@ -70,17 +69,17 @@ public class MainPageResponse{
         FindMember reviewWriter;
         FindMember recipeWriter;
     }
+
     @Getter
     @AllArgsConstructor
-    public static class FindRecipe
-    {
+    public static class FindRecipe {
         Long id;
         String title;
     }
+
     @Getter
     @AllArgsConstructor
-    public static class FindMember
-    {
+    public static class FindMember {
         Long id;
         String name;
     }
