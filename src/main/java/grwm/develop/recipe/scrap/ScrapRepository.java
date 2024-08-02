b7e893b8e53f809bae004702215773f81e4d9684
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-08-02 11:28:45 +0900

    🔨 refactor: 함수추가
    ScrapRepository

--- src/main/java/grwm/develop/recipe/scrap/ScrapRepository.java
+++ src/main/java/grwm/develop/recipe/scrap/ScrapRepository.java
@@ -2,7 +2,9 @@ package grwm.develop.recipe.scrap;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
-     Scrap findByMemberId(Long memberId);
      boolean existsByMemberIdAndRecipeId(Long memberId, Long recipeId);
+     List<Scrap> findAllByMemberId(Long memberId);
 }
