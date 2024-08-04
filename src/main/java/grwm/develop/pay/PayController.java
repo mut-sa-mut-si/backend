4711a2e306002fb6fffd08966b67e84f63f67a20
ayeon598 <165542857+ayeon598@users.noreply.github.com>
2024-08-04 15:15:20 +0900

    🔨 refactor: PostMapping으로 수정

--- src/main/java/grwm/develop/pay/PayController.java
+++ src/main/java/grwm/develop/pay/PayController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +20,7 @@ public class PayController {
 
     private final PayService payService;
 
-    @GetMapping
+    @PostMapping
     public String beforePay(@RequestBody PayInfoRequest request,
                             @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return "redirect:" + payService.beforePay(request, userDetails.member());
