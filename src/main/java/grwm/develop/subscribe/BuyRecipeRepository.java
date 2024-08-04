9dbed44ee5aed0452de55f77e7b2849c489fee13
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-08-04 22:22:55 +0900

    🔨 refactor:: 리포지토리에 함수 추가

--- src/main/java/grwm/develop/subscribe/BuyRecipeRepository.java
+++ src/main/java/grwm/develop/subscribe/BuyRecipeRepository.java
@@ -3,4 +3,5 @@ package grwm.develop.subscribe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BuyRecipeRepository extends JpaRepository<buyRecipe, Integer> {
+    public boolean existsByMemberIdAndRecipeId(Long memberId, Long recipeId);
 }
