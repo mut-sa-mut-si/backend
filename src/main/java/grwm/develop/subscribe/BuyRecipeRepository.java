d7966ea82bf2941eb32d96473b02719c609934eb
rkrzy <81816761+rkrzy@users.noreply.github.com>
2024-08-04 21:57:57 +0900

    🚀 feat: buyRecipe리포지토리 추가

---
+++ src/main/java/grwm/develop/subscribe/BuyRecipeRepository.java
@@ -0,0 +1,6 @@
+package grwm.develop.subscribe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuyRecipeRepository extends JpaRepository<buyRecipe, Integer> {
+}
